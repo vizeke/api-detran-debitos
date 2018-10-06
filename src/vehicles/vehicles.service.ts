f860c2010a08c31074d3a4c40e71f2179c9efd33
Dérick Hogan Pimenta <derickhoganpimenta@yahoo.com.br>
2018-10-06 17:10:47 -0300

    XMLBuilder

--- src/vehicles/vehicles.service.ts
+++ src/vehicles/vehicles.service.ts
@@ -7,7 +7,27 @@ export class VehiclesService {
   private readonly url = 'http://requestbin.fullcontact.com/u4yaidu4';
 
   async getTickets(place, owner_document, vehicle_document): Promise<String> {
-    var xml = '<?xml version="1.0" encoding="utf-8"?><soap:Envelope xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xmlns:xsd="http://www.w3.org/2001/XMLSchema" xmlns:soap="http://schemas.xmlsoap.org/soap/envelope/">  <soap:Header>    <SegurancaDetran xmlns="http://tempuri.org/">      <Usuario>'+process.env.DETRAN_USER+'</Usuario>      <Senha>'+process.env.DETRAN_PASS+'</Senha>    </SegurancaDetran>  </soap:Header>  <soap:Body>    <ObterDebitos xmlns="http://tempuri.org/">      <veiculoConsulta>        <Placa>ABC1234</Placa>       <CPF>12345678910</CPF>        <Renavam>10987654321</Renavam>      </veiculoConsulta>    </ObterDebitos>  </soap:Body></soap:Envelope>'
+    var xml = builder.create('root', {encoding: "utf-8"})
+    .ele('soap:Envelope', {"xmlns:xsi": "http://www.w3.org/2001/XMLSchema-instance", "xmlns:xsd": "http://www.w3.org/2001/XMLSchema", "xmlns:soap": "http://schemas.xmlsoap.org/soap/envelope/"})
+      .ele('soap:Header')
+        .ele("SegurancaDetran", {"xmlns": "http://tempuri.org/"})
+          .ele("Usuario", process.env.DETRAN_USER).up()
+          .ele("Senha", process.env.DETRAN_PASS).up()
+        .up()
+      .up()
+      .ele('soap:Body')
+        .ele('ObterDebitos', {"xmlns": "http://tempuri.org/"})
+          .ele('veiculoConsulta')
+          .ele("Placa", place).up()
+          .ele("CPF", owner_document).up()
+          .ele("Renavam", vehicle_document).up()
+          .up()
+        .up()
+      .up()
+    .end({ pretty: true});
+
+    console.log(xml)
+
     const options = {
           uri: this.url,
           body: xml,
