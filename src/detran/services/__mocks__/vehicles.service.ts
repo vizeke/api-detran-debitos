a0b02f31abc2c4bd42d7257686ff7f88ca7a87cc
Mateus Garcia <mateus.gigainfo@gmail.com>
2018-10-25 01:10:04 -0300

    test(searchVehicle): teste unitario para servir de exemplo

--- src/detran/services/__mocks__/vehicles.service.ts
+++ src/detran/services/__mocks__/vehicles.service.ts
@@ -5,45 +5,45 @@ import { Injectable } from '@nestjs/common';
 
 @Injectable()
 export class VehiclesService {
-  async searchVehicle(plate, owner_document){
+  async searchVehicle ( plate, owner_document ) {
 
-    const respostaVeiculoEncontrado =  {
-        ObterDadosVeiculoResult: {
-          VeiculoInfo: {
-            Veiculo: {
-              Placa: 'VAL1705',
-              CPF: 9876543210,
-              Renavam: 10987654321,
-            },
-            Nome: 'JOSE SILVA',
-            MarcaModelo: 'VW/PARATI 1.6 TRACKFIELD',
-            AnoFabricacao: 2006,
+    const respostaVeiculoEncontrado = {
+      ObterDadosVeiculoResult: {
+        VeiculoInfo: {
+          Veiculo: {
+            Placa: 'VAL1705',
+            CPF: 9876543210,
+            Renavam: 10987654321,
           },
+          Nome: 'JOSE SILVA',
+          MarcaModelo: 'VW/PARATI 1.6 TRACKFIELD',
+          AnoFabricacao: 2006,
         },
-      };
+      },
+    };
 
-    const respostaVeiculoRoubado =  {
+    const respostaVeiculoRoubado = {
       ObterDadosVeiculoResult: {
         MensagemErro: 'Consulta não permitida para veículo com registro de furto/roubo ativo',
       },
     };
 
-    const respostaVeiculoNEncontrado =  {
+    const respostaVeiculoNEncontrado = {
       ObterDadosVeiculoResult: {
-        MensagemErro: 'Veículo placa XXX0000 e CPF/CNPJ 00000000000 não encontrado.',
+        MensagemErro: 'Veículo não encontrado.',
       },
     };
 
-    if (plate === 'VAL1705' && owner_document === '9876543210'){
+    if ( plate === 'VAL1705' && owner_document === '9876543210' ) {
       return respostaVeiculoEncontrado;
-    }else if (plate === 'ROU8470' && owner_document === '12345678910'){
+    } else if ( plate === 'ROU8470' && owner_document === '12345678910' ) {
       return respostaVeiculoRoubado;
-    }else{
+    } else {
       return respostaVeiculoNEncontrado;
     }
   }
 
-  async getTickets(plate, owner_document): Promise<any> {
+  async getTickets ( plate, owner_document ): Promise<any> {
 
     const resposta = {
     };
--- src/detran/services/vehicles.service.spec.ts
+++ src/detran/services/vehicles.service.spec.ts
@@ -1,15 +1,48 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { VehiclesService } from './vehicles.service';
+jest.mock( './vehicles.service' );
 
-describe('VehiclesService', () => {
+describe( 'VehiclesService', () => {
   let service: VehiclesService;
-  beforeAll(async () => {
-    const module: TestingModule = await Test.createTestingModule({
-      providers: [VehiclesService],
-    }).compile();
-    service = module.get<VehiclesService>(VehiclesService);
-  });
-  it('should be defined', () => {
-    expect(service).toBeDefined();
-  });
-});
+  beforeAll( async () => {
+    const module: TestingModule = await Test.createTestingModule( {
+      providers: [ VehiclesService ],
+    } ).compile();
+    service = module.get<VehiclesService>( VehiclesService );
+  } );
+
+
+
+  it( 'SearchVehicle() com dados válidos deve retornar dados do proprietário', async () => {
+    let plate = 'VAL1705';
+    let owner_document = '9876543210';
+    let respostaDoTeste = await service.searchVehicle( plate, owner_document );
+    expect( respostaDoTeste.ObterDadosVeiculoResult.VeiculoInfo.Nome )
+      .toBe( "JOSE SILVA" );
+  } );
+
+
+
+  it( 'SearchVehicle() com dados errados deve retornar mensagem de erro', async () => {
+    let plate = 'arrays começam em 1';
+    let owner_document = 'usem goto! é legal sim abiguinhos';
+    let respostaDoTeste = await service.searchVehicle( plate, owner_document );
+    expect( respostaDoTeste.ObterDadosVeiculoResult.MensagemErro )
+      .toBe( 'Veículo não encontrado.' );
+  } );
+
+
+  it( 'SearchVehicle() com dados de veículo roubado deve impedir a consulta', async () => {
+    let plate = 'ROU8470';
+    let owner_document = '12345678910';
+    let respostaDoTeste = await service.searchVehicle( plate, owner_document );
+    expect( respostaDoTeste.ObterDadosVeiculoResult.MensagemErro )
+      .toBe( 'Consulta não permitida para veículo com registro de furto/roubo ativo' );
+  } );
+
+
+  //FAÇAM OS TESTES DO OUTRO MÉTODO SEGUINDO O EXEMPLO ACIMA.
+
+
+
+} );
