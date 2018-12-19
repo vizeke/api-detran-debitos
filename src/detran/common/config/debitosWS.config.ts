36910a3ddfbab5ac81268ad6c11cb7c3dab3e232
Marcos A C de Paula <cardepaula@gmail.com>
2018-12-19 18:04:28 -0200

    feat(gui): ValidaIPVA() validando cotas

--- src/detran/common/config/debitosWS.config.ts
+++ src/detran/common/config/debitosWS.config.ts
@@ -1,12 +1,6 @@
-let wsurl: string;
+const wsurl: string = process.env.DETRAN_URL;
 const userWS: string = process.env.DETRAN_USER;
 const passWS: string = process.env.DETRAN_PASS;
-
-if ( process.env.NODE_ENV !== 'production'){
-    wsurl = 'http://novo.detrannet.dchm.es.gov.br/wsInternetbanking/serviceInternetBanking.asmx?wsdl';
-}else {
-    wsurl = 'https://api.detran.es.gov.br/wsinternetbanking/serviceInternetBanking.asmx?wsdl';
-}
 
 export class DebitosWS {
     serviceUrl: string;
